226d0446da39db06b20a6a06eec3e1152302adff
alexandregranzerguay <alexandre.granzerguay@gmail.com>
2017-07-05 12:32:47 -0700

    Update All Files

--- client/app.js
+++ client/app.js
@@ -1,23 +1,42 @@
-var myApp = angular.module("myApp", ['ngRoute']);
+var myApp = angular.module("myApp", []);
 
-myApp.config(function($routeProvider){
-  $routeProvider.when('/', {
-    // TO TEST WHEN EVERYTHING ELSE WORKS
-    // redirectTo:'/'
-    controller:'mikaController',
-    templateUrl:'index.html'
-  })
-  $routeProvider.when('/answer', {
-    // TO TEST WHEN EVERYTHING ELSE WORKS
-    // redirectTo:'/'
-    controller:'mikaController',
-    templateUrl:'index.html'
-  })
-  // .when('/answer', {
-  //   controller:'mikaController',
-  //   templateUrl:'views/answer.html'
-  // })
-  // .otherwise({
-  //   redirectTo: '/'
-  // });
-});
+// myApp.config(['$routeProvider', function($routeProvider){
+//   $routeProvider.when('/', {
+//     // TO TEST WHEN EVERYTHING ELSE WORKS
+//     controller:'mikaController',
+//     // templateUrl:'/client/index.html'
+//   })
+//   $routeProvider.when('/answer', {
+//     // TO TEST WHEN EVERYTHING ELSE WORKS
+//     controller:'mikaController',
+//     templateUrl:'/client/index.html'
+//   })
+//   // .when('/answer', {
+//   //   controller:'mikaController',
+//   //   templateUrl:'views/answer.html'
+//   // })
+//   // .otherwise({
+//   //   redirectTo: '/'
+//   // });
+// }]);
+
+myApp.controller("mikaController",['$scope', '$http', function($scope, $http){
+  console.log('messaging loaded')
+  // $scope.mika = { answer: 'Test' };
+
+  $scope.askMika = function() {
+    console.log('send button was pressed')
+    console.log($scope.question)
+    $http.post('/api/answer', $scope.question)
+      .then(function successCallback(response){
+        console.log('Got answer')
+        console.log(response)
+        // $scope.mika = { answer: response };
+        $scope.mika = response;
+      }, function errorCallback(error){
+        console.log('got call back')
+        console.log(error);
+      });
+  };
+    // $http.post('/someUrl', data, config).then(successCallback, errorCallback);
+}]);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var cfenv = require('cfenv');
 var bodyParser = require('body-parser');
 var Conversation = require('watson-developer-cloud/conversation/v1');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ 'extended': false })
 var prompt = require('prompt-sync')();
 var readlineSync = require('readline-sync');
 
@@ -14,32 +14,61 @@ var conversation = new Conversation({
   version_date: '2016-07-11'
 });
 var context = {};
-
+// Get our API routes
+// var api = require('./client/app');
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use('/client', express.static(__dirname + '/client'));
+app.use('/controllers', express.static(__dirname + '/client/controllers'));
+app.use('/css', express.static(__dirname + '/client/css'));
+app.use('/angular', express.static(__dirname + '/client/bower_components/angular'));
+app.use('/angular-route', express.static(__dirname + '/client/bower_components/angular-route'));
+app.use('/jquery', express.static(__dirname + '/client/bower_components/jquery'));
+app.use('/bootstrap', express.static(__dirname + '/client/bower_components/bootstrap'));
+// app.use('/answer', api)
 
-app.get('/', function(req, res){
+app.get('*', function(req, res){
   res.sendFile(__dirname + '/client/index.html')
 })
 
-app.get('/answer', function(req, res){
+app.get('/api/answer', function(req, res){
+  console.log('entered request')
+    msg={
+      message:req.body
+    };
+    // Start conversation with empty message.
+    console.log(msg.message)
+    conversation.message({'input': msg , 'context':context}, function(err, response){
+      if (err) {
+        console.log('error:', err)
+      }
+      else {
+        console.log(JSON.stringify(response.output.text[0]))
+        var context = response.context;
+        res.json(response.output.text[0])
+      }
+    });
+})
+
+app.post('/api/answer', function(req, res){
     msg={
       message:req.query.message
     };
     // Start conversation with empty message.
     console.log(msg.message)
-    conversation.message({input:msg.message, context:context}, function(err, response){
+    conversation.message({'input': msg , 'context':context}, function(err, response){
       if (err) {
+        console.log(msg)
         console.log('error:', err)
       }
       else {
-        console.log(JSON.stringify(response))
+        response.output.text[0].replace(/"/g, '')
+        console.log(JSON.stringify(response.output.text[0]))
         var context = response.context;
-        res.send(JSON.stringify(response))
+        res.send(JSON.stringify(response.output.text[0]))
       }
     });
 })
-
 // app.post('/api/message', function(req,res){
 //   var payload = {
 //     context: req.body.context || {},
